ef6426b8f99afa2a1eefa054cafea37ae392e157
Ravi <ravitripathi@devopss-Mac-mini.local>
2025-04-23 22:40:22 +0530

    adding irsa

--- modules/eks/access-entry.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_eks_access_entry" "eks-terraform" {
-  cluster_name      = aws_eks_cluster.eks-terraform.name
-  principal_arn     = aws_iam_role.example.arn
-  kubernetes_groups = ["group-1", "group-2"]
-  type              = "STANDARD"
-}
