ee1f0d4b42dba3f9604965bdc938408d4a46b009
Ravi <prakash.ravi144@gmail.com>
2025-05-07 22:11:52 +0530

    albc

--- us-region/providers.tf
+++ us-region/providers.tf
@@ -10,3 +10,15 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "arn:aws:eks:us-east-1:053097998829:cluster/eks-with-terraform"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path    = "~/.kube/config"
+    config_context = "arn:aws:eks:us-east-1:053097998829:cluster/eks-with-terraform"
+  }
+}
