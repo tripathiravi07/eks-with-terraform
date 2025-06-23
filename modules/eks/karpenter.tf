7d24908f9a0a3f330d0ca5b09ac0a8aa2994f409
Ravi <prakash.ravi144@gmail.com>
2025-06-23 15:40:25 +0530

    adding

--- modules/eks/karpenter.tf
+++ modules/eks/karpenter.tf
@@ -92,7 +92,8 @@ locals {
   node_policies = [
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
+    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   ]
 }
 
