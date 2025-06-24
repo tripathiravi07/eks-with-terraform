46f3a8fd80a65c5092b62cb1cf7bbf6a91e0c886
Ravi <prakash.ravi144@gmail.com>
2025-06-24 12:05:49 +0530

    providing fix

--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -22,3 +22,7 @@ output "eks_cluster_ca" {
 output "eks_cluster_name" {
     value = aws_eks_cluster.eks-terraform.name
 }
+
+output "eks_cluster_version" {
+    value = aws_eks_cluster.eks-terraform.platform_version
+}
