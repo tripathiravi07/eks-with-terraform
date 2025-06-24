9beb2b7d50a1b9c4334f314186ab9c4a0d087b87
Ravi <prakash.ravi144@gmail.com>
2025-06-24 12:15:22 +0530

    removing output

--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -22,7 +22,3 @@ output "eks_cluster_ca" {
 output "eks_cluster_name" {
     value = aws_eks_cluster.eks-terraform.name
 }
-
-output "eks_cluster_version" {
-    value = aws_eks_cluster.eks-terraform[0].platform_version
-}
